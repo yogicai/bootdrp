cc56df8adacddce4ebf8439a79327ad0e51f04df
lbwahoo <lbwahoo@163.com>
2026-08-11 18:32:20 +0800

    升级jdk21

--- src/main/java/com/bootdo/modular/system/controller/BaseController.java
+++ src/main/java/com/bootdo/modular/system/controller/BaseController.java
@@ -2,19 +2,14 @@ package com.bootdo.modular.system.controller;
 
 import com.bootdo.core.utils.SecurityUtils;
 import com.bootdo.modular.system.domain.UserDO;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  * @author L
  */
 @Controller
 public class BaseController {
+
     public UserDO getUser() {
         return SecurityUtils.getUser();
     }
@@ -27,8 +22,4 @@ public class BaseController {
         return getUser().getUsername();
     }
 
-    @InitBinder
-    public void init(WebDataBinder binder) {
-        binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd"), true));
-    }
 }
